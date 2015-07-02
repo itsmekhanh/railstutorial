a434386c86dc9ce07783d446d665fd11467d8c22
Khanh Luc <itsmekhanh@gmail.com>
2015-07-01 19:48:35 -0700

    started chapter 4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
@@ -19,7 +19,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   end
 
   test "should get about" do
-    get :help
+    get :about
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get :about
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
