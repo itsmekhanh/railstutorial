aa6ea5d0f939bf4c5ae2cace490cfd0a09110988
Khanh Luc <itsmekhanh@gmail.com>
2015-07-29 23:31:14 -0700

    updated chapter 7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                      }
     end
     assert_template "users/new"
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
   end
 
   test "valid signup information" do
@@ -26,5 +28,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                   }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
